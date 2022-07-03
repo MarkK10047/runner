5203b1929e4a41d8328d9f69964a1c2c09d35be2
Mark <ataevmarko@gmail.com>
2022-07-03 14:56:13 +0600

    finished carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-  $('.carousel__inner').slick();
+  $('.carousel__inner').slick({
+    speed: 1000,
+    prevArrow: '<button type="button" class="slick-prev"><img src="./img/slider/left-arrow.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="./img/slider/right-arrow.png"></button>',
+    draggable: false
+  });
 });
