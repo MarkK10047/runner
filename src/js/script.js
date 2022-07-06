341d4c3bf8248751eb993ce4edb66842aa3a6d8a
Mark <ataevmarko@gmail.com>
2022-07-06 13:32:20 +0600

    finished the catalog section and js file for tabs and desc link

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,21 @@ $(document).ready(function(){
     nextArrow: '<button type="button" class="slick-next"><img src="./img/slider/right-arrow.png"></button>',
     draggable: false
   });
+  $('.catalog__tabs').on('click', 'div:not(catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__products').removeClass('catalog__products_active').eq($(this).index()).addClass('catalog__products_active');
+  });
+  function toogleSlide(item){
+    $(item).each(function(i){
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog__info').eq(i).toggleClass('catalog__info_active')
+        $('.catalog__desc').eq(i).toggleClass('catalog__desc_active')
+      })
+    })
+  }
+
+  toogleSlide(".catalog__link-desc")
+  toogleSlide(".catalog__link-back")
 });
